fdd07eaab8614d7d07fa1d3b4fa78ec05b22c835
Patricia Geraci <limmagin@hotmail.fr>
2016-12-08 16:14:46 +0100

    Ajout fichiers

---
+++ menuformulaire.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="fr-FR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="fr-FR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title> Planning</title>
+	<link rel='stylesheet'  href='menuformulaire.css' type='text/css' />
+	<meta name='planning' content='' />
+	</head>
+
+  <body>
+  <div class="Adherent"><h1>Adhérents</h1><br />
+	</div>
+	<div class="menu">
+						<a href "      ? page = Profil.php" id="Profil">Profil</a>
+						<a href "" id="Contact">Contact</a>
+						<a href "" id="Resa">Résa</a>
+						<a href "" id="Equipe">Equipes</a>
+	</div>
+<div class="includ">
+//	<?php
+//	include="ma variable $session"
+
+//	 ?>
+</div>
+
+  </body>
+</html>
